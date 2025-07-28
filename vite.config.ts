016cf2175c42abe56f25480f7318d9d89a9e24e8
PEEKPerformer <brendenferland@gmail.com>
2025-07-28 16:57:52 -0400

    latest

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import Pages from 'vite-plugin-pages';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    Pages({
+      dirs: [{ dir: 'src/artifacts', baseRoute: '' }],
+      extensions: ['jsx', 'tsx'],   
+    }),
+  ],
+  base: '/2025IMSACS/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'src': path.resolve(__dirname, './src'),
+    },
+  }
+})
